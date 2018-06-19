98ab71b8f5b21253849e4ee8eafb8df39f5adaed
Jilcimar Fernandes <jilcimar.fernandes0267@gmail.com>
2018-06-19 10:31:37 -0300

    Update qst2.cpp

--- algoritmos/listas/2 - UNIDADE/L3/qst2.cpp
+++ algoritmos/listas/2 - UNIDADE/L3/qst2.cpp
@@ -1,35 +1,28 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main ()
 {
-    int tamanho;
-    cout << "Insira o tamanho do seu vetor:";
-    cin >> tamanho;
-    int v[tamanho];
+    int v[3];
+    int v2[3];
 
-    for(int i = 0 ; i < tamanho; i++)
+    for(int i = 0 ; i < 3; i++)
     {
-        cout << "Insira o " << (i+1) << " número:";
+        cout << "Insira o " << (i+1) << " coordenada do vetor 1:";
         cin >> v[i];
-    }
-
-    int tamanho2;
-    cout << "Insira o tamanho do seu 2° vetor:";
-    cin >> tamanho2;
-    int v2[tamanho2];
-
-    for(int i = 0 ; i < tamanho2; i++)
-    {
-        cout << "Insira o " << (i+1) << " número:";
+        cout << "\nInsira o " << (i+1) << " coordenada do vetor 2:";
         cin >> v2[i];
     }
+    float coordenada1 = (pow(2,(v2[0]-v[0])));
+    float coordenada2 = (pow(2,(v2[1]-v[1])));
+    float coordenada3 = (pow(2,(v2[2]-v[2])));
 
-
-    //calculando a distância
-
+    float distancia = coordenada1 + coordenada2  + coordenada3 ;
     
-    
+    cout.precision(2);
+    cout << "A distância entre os dois pontos é:" << sqrt(distancia) <<" \n";
+
     return 0;
 }
